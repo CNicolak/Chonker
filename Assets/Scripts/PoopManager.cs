c6059303e1a91b5b15a07e4e6ddb61c2de4411e6
MichaelBarreiros <48330508+MichaelBarreiros@users.noreply.github.com>
2020-04-05 21:59:33 -0400

    Adding the ability to have and delete multiple poops

--- Assets/Scripts/PoopManager.cs
+++ Assets/Scripts/PoopManager.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 
 public class PoopManager : MonoBehaviour
 {
-    public GameObject poop;
+    public GameObject[] poops = new GameObject[5];
     public GameObject poopObject;
     public GameObject manager;
-    
-    [SerializeField]    
-    private bool _poopLocation;     // 0 or 1
 
+    private int _poopLocation;    // 0 or 1
+    private int[] _poopLocations = new int[5];
+    private const int MAX_POO = 5;
 
+    public void Start() {
+        for(int i = 0; i < MAX_POO; i++){
+            poops[i] = null;
+            _poopLocations[i] = 3;
+        }
+    }
     public void generatePoop(){
        //if(poop){
             //Destroy(poop);
@@ -24,25 +30,45 @@ public class PoopManager : MonoBehaviour
         Vector3 position;
         if (inLitter) {
             position = new Vector3(-5.15f, 0f, 0f);
-            _poopLocation = true;
+            _poopLocation = 1;
             Debug.Log("Poop in litter");
         }
         else {
             position = new Vector3(5.15f, 0f, 0f);
-            _poopLocation = false;
+            _poopLocation = 0;
             Debug.Log("Poop not in litter");
         }
 
-        poop = Instantiate(poopObject, position, Quaternion.identity) as GameObject;
+        int i;
+        for(i = 0; i < MAX_POO; i++){
+            if (poops[i] == null) break;
+        }
+        if (i == 5){ //We have max poo
+            //trigger clean 
+        }
+        _poopLocations[i] = _poopLocation;
+        poops[i] = Instantiate(poopObject, position, Quaternion.identity) as GameObject;
     }
     
-    public bool poopLocation{
+    public int poopLocation{
         get{ return _poopLocation; }
     }
     
     public void cleanPoop(Button button){
-        //button.interactable = false;
-        Destroy(poop);
+        int location = MAX_POO;
+        if (button.CompareTag("litterBox")) {
+            for(location = MAX_POO - 1; location >= 0; location--){
+                if (_poopLocations[location] == 1) break;
+            }
+            Destroy(poops[location]);
+        } else if (button.CompareTag("notLitterBox")){
+            for (location = MAX_POO -1; location >= 0; location--) {
+                if (_poopLocations[location] == 0) break;
+            }
+            Destroy(poops[location]);
+        }
+        _poopLocations[location] = 3;
+
         manager.GetComponent<GameManager>().triggerPunishPraisePanel(0);
     }
 }
