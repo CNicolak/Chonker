1e38fc0c8addbad6e339f3fab88f588e9d786caa
MichaelBarreiros <48330508+MichaelBarreiros@users.noreply.github.com>
2020-04-05 22:51:47 -0400

    MAX_POOP is now 10, buttons are not interactable unless poop is on them

--- Assets/Scripts/PoopManager.cs
+++ Assets/Scripts/PoopManager.cs
@@ -5,20 +5,21 @@ using UnityEngine.UI;
 
 public class PoopManager : MonoBehaviour
 {
-    public GameObject[] poops = new GameObject[5];
+    public GameObject[] poops = new GameObject[10];
     public GameObject poopObject;
     public GameObject manager;
 
     private int _poopLocation;    // 0 or 1
-    private int[] _poopLocations = new int[5];
-    private const int MAX_POO = 5;
+    private int[] _poopLocations = new int[] {3,3,3,3,3,3,3,3,3,3};
+    private const int MAX_POO = 10;
 
-    public void Start() {
+    /**public void Start() {
+        Debug.Log("POOP MANAGER START CALLED");
         for(int i = 0; i < MAX_POO; i++){
             poops[i] = null;
             _poopLocations[i] = 3;
         }
-    }
+    }*/
     public void generatePoop(){
        //if(poop){
             //Destroy(poop);
@@ -56,18 +57,26 @@ public class PoopManager : MonoBehaviour
     
     public void cleanPoop(Button button){
         int location = MAX_POO;
+        bool found = false;
+        bool anotherPoopInBox = false;
         if (button.CompareTag("litterBox")) {
-            for(location = MAX_POO - 1; location >= 0; location--){
-                if (_poopLocations[location] == 1) break;
+            for(int i = MAX_POO - 1; i >= 0; i--){
+                if (!found && _poopLocations[i] == 1) { Debug.Log("POOP IS IN LITTER: " + i); found = true; location = i; continue; }
+                if (found && _poopLocations[i] == 1) { Debug.Log("ANOTHER POOP IS HERE"); anotherPoopInBox = true; break; } 
             }
-            Destroy(poops[location]);
         } else if (button.CompareTag("notLitterBox")){
-            for (location = MAX_POO -1; location >= 0; location--) {
-                if (_poopLocations[location] == 0) break;
+            for (int i = MAX_POO - 1; i >= 0; i--) {
+                if (!found && _poopLocations[i] == 0) { Debug.Log("POOP IS NOT IN LITTERBOX: " + i); found = true; location = i; continue; }
+                if (found && _poopLocations[i] == 0) { Debug.Log("ANOTHER POOP IS HERE"); anotherPoopInBox = true; break; }
             }
-            Destroy(poops[location]);
         }
-        _poopLocations[location] = 3;
+        if (found) {
+            _poopLocations[location] = 3;
+            Destroy(poops[location]);
+            if (!anotherPoopInBox) button.interactable = false;
+        }else{
+            button.interactable = false;
+        }
 
         manager.GetComponent<GameManager>().triggerPunishPraisePanel(0);
     }
