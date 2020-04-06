339f149d5ebc5a584cf375b4aa7f3052ce65f003
CNicolak <CNicolak@users.noreply.github.com>
2020-04-06 00:16:27 -0400

    Added Pet Assets for skins
    Updated Pet.cs

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -4,40 +4,37 @@ using System.Collections;
 
 public class Pet : MonoBehaviour {
 
-    [SerializeField]    
-    private int _happiness;     // 100 to 0
-
-    [SerializeField]
-    private int _hunger;        // 100 to 0
-
-    [SerializeField]
-    private int _health;        // 100 to 0
-
+    // Initialization
     [SerializeField] 
     private string _name;
-
     [SerializeField]
-    private int _discipline;        // 100 to 0
-
+    private int _health;       // 0 to 100
+    [SerializeField]    
+    private int _happiness;    // 0 to 100
     [SerializeField]
-    private int _waste;        // 10 to 0
+    private int _hunger;      // 0 to 100
+    [SerializeField]
+    private int _discipline;   // 0 to 100
+    [SerializeField]
+    private int _waste;       // 0 to 10
+    [SerializeField]
+    private int _age;        // 0 to 5 (Egg, Baby, Child, Adult, Senior, Dead)
 
     private bool _serverTime;
     private int _clickCount;
 
     void Start() {
-
         //PlayerPrefs.SetString("then", "03/27/2020 17:00:00"); //Debugging
         updateStatus();
 
         if(!PlayerPrefs.HasKey("name"))
             PlayerPrefs.SetString("name", "Chonker");
         _name = PlayerPrefs.GetString ("name");
-
     }
 
     void Update(){
 
+        // Allows user to clikc pet.
         // If obj is above 2.9f, Jump is true.
         GetComponent<Animator>().SetBool("Jump", gameObject.transform.position.y >-2.9f);
 
@@ -51,7 +48,7 @@ public class Pet : MonoBehaviour {
                     _clickCount++;
                     if(_clickCount >= 3){
                         _clickCount = 0;
-                        updateHappiness(1);
+                        //updateHappiness(1);
                         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000000));
                     }
                 }
@@ -60,13 +57,14 @@ public class Pet : MonoBehaviour {
     }
 
 
-    void updateStatus() { // access saves
+    void updateStatus() { 
 
-        if(!PlayerPrefs.HasKey("_hunger")){ 
-            _hunger = 100;
-            PlayerPrefs.SetInt("_hunger", _hunger);
+        // Access saves to update meters. Adds initial value for first load.
+        if(!PlayerPrefs.HasKey("_health")){ 
+            _health = 100;
+            PlayerPrefs.SetInt("_health", _health);
         } else {
-            _hunger = PlayerPrefs.GetInt("_hunger");
+            _health = PlayerPrefs.GetInt("_health");
         }
 
         if(!PlayerPrefs.HasKey("_happiness")){
@@ -74,6 +72,13 @@ public class Pet : MonoBehaviour {
             PlayerPrefs.SetInt("_happiness", _happiness);
         } else {
             _happiness = PlayerPrefs.GetInt("_happiness");
+        }
+
+        if(!PlayerPrefs.HasKey("_hunger")){ 
+            _hunger = 100;
+            PlayerPrefs.SetInt("_hunger", _hunger);
+        } else {
+            _hunger = PlayerPrefs.GetInt("_hunger");
         }
 
         if(!PlayerPrefs.HasKey("_discipline")){ 
@@ -91,7 +96,7 @@ public class Pet : MonoBehaviour {
         }
 
 
-
+        // Checks the time the last time the game was opened.
         if(!PlayerPrefs.HasKey("then"))
             PlayerPrefs.SetString("then", getStringTime());
 
@@ -141,44 +146,38 @@ public class Pet : MonoBehaviour {
         return now.Month + "/" + now.Day + "/" + now.Year + " " + now.Hour + ":" + now.Minute + ":" + now.Second;
     }
 
-
+    // Initialization
     public int hunger{
         get{ return _hunger; }
         set{ _hunger = value; }
     }
-
     public int happiness{
         get{ return _happiness; }
         set{ _happiness = value; }
     }
-
     public int health{
         get{ return _health; }
         set{ _health = value; }
     }
-
     public int discipline{
         get{ return _discipline; }
         set{ _discipline = value; }
     }
-
     public int waste{
         get{ return _waste; }
         set{ _waste = value; }
     }
-
     public string name{
         get{ return _name; }
         set{ _name = value; }
     }
 
-    // Increase/Decrease Happiness Meter
+    // Update Meters. Add boundaries/range.
     public void updateHappiness(int i){
         happiness += i;
         if(happiness > 100)
             happiness = 100;
     }
-
     public void updateHunger(int i){
         hunger += i;
         if(hunger > 100) {
@@ -187,9 +186,6 @@ public class Pet : MonoBehaviour {
             hunger = 0;
         }
     }
-
-
-    // Increase/Decrease Discipline Meter
     public void updateDiscipline(int i){
         discipline += i;
         if(discipline > 100)
@@ -198,8 +194,6 @@ public class Pet : MonoBehaviour {
         Debug.Log(discipline);            
     }
 
-
-    // Increase/Decrease Waste Meter
     public void updateWaste(int i){
         waste += i;
         if(waste > 10)
@@ -208,9 +202,12 @@ public class Pet : MonoBehaviour {
         Debug.Log(waste);            
     }
 
+
+    // Saves the pet data to PlayerPrefs
     public void savePet(){
         if (!_serverTime)
             updateDevice(); // Store in PlayerPrefs
+
         PlayerPrefs.SetInt("_hunger", _hunger);
         PlayerPrefs.SetInt("_happiness", _happiness); 
         PlayerPrefs.SetInt("_health", _health);
